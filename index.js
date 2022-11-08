55c386a1314f700a65f7383f42796771a50c301d
MD Pahlovi <mdpahlovi07@gmail.com>
2022-11-09 02:04:42 +0600

    Data Load by Id added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("colors");
 require("dotenv").config();
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,11 +19,19 @@ const client = new MongoClient(url);
 const fun = async () => {
     await client.connect();
     const servicesCategories = client.db("photoGrapher-services").collection("categories");
+    // Get Data From Server
     app.get("/services", async (req, res) => {
         const showLimit = parseInt(req.query.limit);
         const curser = servicesCategories.find({});
         const categories = await curser.limit(showLimit).toArray();
         res.send(categories);
+    });
+    //Get Data by id
+    app.get("/service/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await servicesCategories.findOne(query);
+        res.send(service);
     });
 };
 fun().catch((error) => console.log(`${error.name.bgRed.bold}: ${error.message.red}`));
