116af73a9dda8973124199e41c5764ac2bddb8a5
MD Pahlovi <mdpahlovi07@gmail.com>
2022-11-08 16:08:09 +0600

    Server Connected & Loaded Data from server

--- index.js
+++ index.js
@@ -14,4 +14,18 @@ app.get("/", (req, res) => {
     res.send("Pahlovi Server Running Okk");
 });
 
+const url = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.v2g7zap.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(url);
+const fun = async () => {
+    await client.connect();
+    const servicesCategories = client.db("photoGrapher-services").collection("categories");
+    app.get("/services", async (req, res) => {
+        const curser = servicesCategories.find({});
+        const categories = await curser.toArray();
+        console.log(categories);
+        res.send(categories);
+    });
+};
+fun().catch((error) => console.log(`${error.name.bgRed.bold}: ${error.message.red}`));
+
 app.listen(port);
