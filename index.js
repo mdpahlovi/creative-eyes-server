d02aeb1994550d18042b3e1ece780e32859330ca
MD Pahlovi <mdpahlovi07@gmail.com>
2022-11-08 13:12:52 +0600

    Server Init

---
+++ index.js
@@ -0,0 +1,17 @@
+require("colors");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middle Wire
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Pahlovi Server Running Okk");
+});
+
+app.listen(port);
