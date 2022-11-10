2eb9f8a0427779cf4dbf0f530ba96fd6f7655bbb
MD Pahlovi <mdpahlovi07@gmail.com>
2022-11-10 15:31:23 +0600

    Update Message

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// JWT verify
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return res.status(401).send({ message: "unauthorized access" });
+        return res.status(401).send({ message: "Unauthorized access" });
     }
     const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
@@ -36,13 +37,15 @@ function verifyJWT(req, res, next) {
 
 const fun = async () => {
     await client.connect();
+    const servicesCategories = client.db("photoGrapher-services").collection("categories");
+    const reviews = client.db("photoGrapher-services").collection("reviews");
+
     app.post("/jwt", (req, res) => {
         const user = req.body;
         const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "2d" });
         res.send({ token });
     });
 
-    const servicesCategories = client.db("photoGrapher-services").collection("categories");
     // Get Services From Server
     app.get("/services", async (req, res) => {
         const showLimit = parseInt(req.query.limit);
@@ -52,7 +55,7 @@ const fun = async () => {
     });
     //Get Service by id
     app.get("/service/:id", async (req, res) => {
-        const id = req.params.id;
+        const { id } = req.params;
         const query = { _id: ObjectId(id) };
         const service = await servicesCategories.findOne(query);
         res.send(service);
@@ -63,24 +66,24 @@ const fun = async () => {
         if (result.insertedId) {
             res.send({
                 success: true,
-                message: "New services Added",
+                message: "New Service Added",
             });
         }
     });
 
-    const reviews = client.db("photoGrapher-services").collection("reviews");
     // Update Review
     app.post("/reviews", async (req, res) => {
         const result = await reviews.insertOne(req.body);
         if (result.insertedId) {
             res.send({
                 success: true,
-                message: "review Updated",
+                message: "New Review Added",
             });
         }
     });
+    // Get Review By Service ID
     app.get("/review/:id", async (req, res) => {
-        const id = req.params.id;
+        const { id } = req.params;
         const query = { serviceId: id };
         const curser = reviews.find(query);
         const thisreviews = await curser.toArray();
@@ -88,7 +91,7 @@ const fun = async () => {
     });
     // Get Product by email
     app.get("/reviewsbyemail", verifyJWT, async (req, res) => {
-        const email = req.query.email;
+        const { email } = req.query;
         const curser = reviews.find({ email });
         const reviewsbyemail = await curser.toArray();
         res.send(reviewsbyemail);
@@ -100,7 +103,7 @@ const fun = async () => {
         if (result.deletedCount) {
             res.send({
                 success: true,
-                message: "Successfully deleted review",
+                message: "Successfully Deleted Review",
             });
         }
     });
