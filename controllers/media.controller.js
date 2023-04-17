39f82dfb74ae4c01cda96a372c87bd086bc4d643
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-04-18 01:17:40 +0600

    Media Update APi Done

--- controllers/media.controller.js
+++ controllers/media.controller.js
@@ -20,3 +20,19 @@ exports.postMedia = async (req, res) => {
     const result = await newMedia.save();
     res.send({ acknowledge: true, insertedId: result._id });
 };
+
+exports.updateBookMedia = async (req, res) => {
+    const media = req.body;
+    const { id } = req.params;
+    const filter = { "booking.id": new ObjectId(id) };
+    const update = {
+        $push: {
+            "media.image": { $each: media.image },
+            "media.audio": { $each: media.audio },
+            "media.video": { $each: media.video },
+        },
+    };
+
+    const result = await Media.findOneAndUpdate(filter, update);
+    res.send({ acknowledge: true, updatedId: result._id });
+};
--- routers/media.route.js
+++ routers/media.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { postMedia, getBookMedia, getUserMedia } = require("../controllers/media.controller");
+const { postMedia, getBookMedia, getUserMedia, updateBookMedia } = require("../controllers/media.controller");
 
 router.get("/user/:id", getUserMedia);
 router.get("/book/:id", getBookMedia);
 router.post("/", postMedia);
+router.patch("/book/:id", updateBookMedia);
 
 module.exports = router;
